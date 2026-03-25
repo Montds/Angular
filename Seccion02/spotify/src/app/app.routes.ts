4acf6ed7b8852cb2dd3b63100893e2036907ee9f
Montds <montilla.edwin.ds@gmail.com>
2026-03-25 12:31:08 -0500

    creacion de seccion modules
    se creo la carpeta de modules y dentro de ella se crearon los modulos auth, history, home, tracks

--- Seccion02/spotify/src/app/app.routes.ts
+++ Seccion02/spotify/src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import {MiEjemplo} from './mi-ejemplo/mi-ejemplo';
 
+
 export const routes: Routes = [
- /*
+  {
+
+  }
+];
+/*
   {
     path:"home",
     loadChildren: () => import("./modules/home/home-module").then(m =>m.HomeModule)
   }
   */
-];
